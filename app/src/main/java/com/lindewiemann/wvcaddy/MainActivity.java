cff3717885b1b16c2635e37da9ef290b9d624416
kamil.sykora.ks <kamil.sykora.ks@gmail.com>
2022-11-23 20:59:18 +0100

    init push

--- app/src/main/java/com/lindewiemann/wvcaddy/MainActivity.java
+++ app/src/main/java/com/lindewiemann/wvcaddy/MainActivity.java
@@ -52,9 +52,9 @@ public class MainActivity extends AppCompatActivity {
         List<ImageButton> btns = find(root, ImageButton.class);
         Button btnSave = (Button) findViewById(R.id.btnSave);
         //android.view.ViewGroup.LayoutParams btnSaveParams = btnSave.getLayoutParams();
-        int iScreenHeighDelta = btnSave.getHeight();
+        int iScreenHeighDelta = 3 * btnSave.getHeight();
 
-        int iHeight = (displayMetrics.heightPixels - (3 * iScreenHeighDelta) - 150) / 2;
+        int iHeight = (displayMetrics.heightPixels - iScreenHeighDelta - 150) / 2;
         if(iHeight < 200) iHeight = 200;
         if(iHeight > 800) iHeight = 800;
         int iWidth = displayMetrics.widthPixels / 5 ;
@@ -62,22 +62,60 @@ public class MainActivity extends AppCompatActivity {
         if(iWidth > 600) iWidth = 600;
 
         for (int i=0; i<btns.size(); i++) {
-            //if(btns.get(i).getTag() != null) {
-                android.view.ViewGroup.LayoutParams params = btns.get(i).getLayoutParams();
-                params.height = iHeight;
-                params.width = iWidth;
 
-                btns.get(i).setLayoutParams(params);
-            //}
+            android.view.ViewGroup.LayoutParams params = btns.get(i).getLayoutParams();
+            params.height = iHeight;
+            params.width = iWidth;
+
+            btns.get(i).setLayoutParams(params);
+
         }
     }
 
     public void removeClick(View view) {
         ImageButton btn = (ImageButton)view;
         int btnId = btn.getId();
-        String strTag = btn.getTag().toString();
 
-        hideParts();
+        hideParts(btnId);
+    }
+
+    public void shiftClick(View view) {
+        Button btn = (Button)view;
+        int btnId = btn.getId();
+
+        Button btnMorning = (Button) findViewById(R.id.btnMorning);
+        Button btnNight = (Button) findViewById(R.id.btnNight);
+        Button btnAfternoon = (Button) findViewById(R.id.btnAfternoon);
+
+        switch(btnId) {
+            case R.id.btnAfternoon:
+                btnMorning.setVisibility(View.GONE);
+                btnNight.setVisibility(View.GONE);
+                break;
+            case R.id.btnNight:
+                btnMorning.setVisibility(View.GONE);
+                btnAfternoon.setVisibility(View.GONE);
+                break;
+            case R.id.btnMorning:
+                btnNight.setVisibility(View.GONE);
+                btnAfternoon.setVisibility(View.GONE);
+                break;
+        }
+
+    }
+
+    public void save(View view) {
+        displayShiftButtons();
+    }
+
+    private void displayShiftButtons() {
+        Button btnMorning = (Button) findViewById(R.id.btnMorning);
+        Button btnNight = (Button) findViewById(R.id.btnNight);
+        Button btnAfternoon = (Button) findViewById(R.id.btnAfternoon);
+
+        btnNight.setVisibility(View.VISIBLE);
+        btnAfternoon.setVisibility(View.VISIBLE);
+        btnMorning.setVisibility(View.VISIBLE);
     }
 
     public static <T extends View> List<T> find(ViewGroup root, Class<T> type) {
@@ -86,9 +124,46 @@ public class MainActivity extends AppCompatActivity {
         return finderByType.getViews();
     }
 
-    private void hideParts() {
-        ViewGroup root = (ViewGroup) findViewById(android.R.id.content);
-        List<LinearLayout> lls = find(root, LinearLayout.class);
+    private void hideParts(int btnId) {
+        Button btnSave = (Button) findViewById(R.id.btnSave);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int iScreenHeighDelta = 3 * btnSave.getHeight();
+        int iHeight = (displayMetrics.heightPixels - iScreenHeighDelta - 150) / 1;
+
+        if(iHeight < 200) iHeight = 200;
+        if(iHeight > 1000) iHeight = 1000;
+        int iWidth = displayMetrics.widthPixels / 2 ;
+        if(iWidth < 150) iWidth = 150;
+        if(iWidth > 800) iWidth = 800;
+
+        LinearLayout llUzsb1l = (LinearLayout) findViewById(R.id.llUzsb1l);
+        LinearLayout llUzsb2l = (LinearLayout) findViewById(R.id.llUzsb2l);
+        LinearLayout llUzsb3l = (LinearLayout) findViewById(R.id.llUzsb3l);
+        LinearLayout llUzsb4l = (LinearLayout) findViewById(R.id.llUzsb4l);
+        LinearLayout llUzsb1r = (LinearLayout) findViewById(R.id.llUzsb1r);
+        LinearLayout llUzsb2r = (LinearLayout) findViewById(R.id.llUzsb2r);
+        LinearLayout llUzsb3r = (LinearLayout) findViewById(R.id.llUzsb3r);
+        LinearLayout llUzsb4r = (LinearLayout) findViewById(R.id.llUzsb4r);
+
+        ImageButton btnUzsb1l = (ImageButton) findViewById(R.id.btnUzsb1l);
+
+        switch(btnId) {
+            case R.id.btnUzsb1l:
+                android.view.ViewGroup.LayoutParams params = btnUzsb1l.getLayoutParams();
+                params.height = iHeight;
+                params.width = iWidth;
+                btnUzsb1l.setLayoutParams(params);
+
+                llUzsb2l.setVisibility(View.GONE);
+                llUzsb3l.setVisibility(View.GONE);
+                llUzsb4l.setVisibility(View.GONE);
+                llUzsb1r.setVisibility(View.GONE);
+                llUzsb2r.setVisibility(View.GONE);
+                llUzsb3r.setVisibility(View.GONE);
+                llUzsb4r.setVisibility(View.GONE);
+                break;
+        }
     }
 
     private static class FinderByType<T extends View> implements LayoutTraverser.Processor {
