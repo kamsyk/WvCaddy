f39313faf6d2dac59e760a8bf3f9ae26d4ddafe7
kamil.sykora.ks <kamil.sykora.ks@gmail.com>
2022-11-29 23:32:52 +0100

    init push

--- app/src/main/java/com/lindewiemann/wvcaddy/ContainerDbHelper.java
+++ app/src/main/java/com/lindewiemann/wvcaddy/ContainerDbHelper.java
@@ -6,7 +6,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class ContainerDbHelper extends SQLiteOpenHelper {
-    public static final int DATABASE_VERSION = 3;
+    public static final int DATABASE_VERSION = 4;
     public static final String DATABASE_NAME = "LwWvCaddy.db";
 
     private static final String SQL_CREATE_ENTRIES =
